0e9ff5afa249790242b911c6ab82a5fd9b827b42
Abdulrazak Idris <Abdulrazakidris93@gmail.com>
2025-05-22 06:56:22 +0100

    added 7-multi_c.js file

---
+++ 7-multi_c.js
@@ -0,0 +1,13 @@
+const { argv } = require('node:process');
+
+// conversion value
+let testValue = 1;
+
+// check for integer.
+if ((argv.length < 3) || ((argv[2] * testValue) != argv[2])) {
+    console.log("Missing number of occurrences");
+} else {
+    for (let i = 0; i < argv[2]; i++) {
+        console.log("C is fun");
+    }
+}
