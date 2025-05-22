06ab872496f13287a82f5dad4011411772fc24e8
Abdulrazak Idris <Abdulrazakidris93@gmail.com>
2025-05-22 06:57:47 +0100

    added Comments

--- 7-multi_c.js
+++ 7-multi_c.js
@@ -3,7 +3,7 @@ const { argv } = require('node:process');
 // conversion value
 let testValue = 1;
 
-// check for integer.
+// check for integer and print C is fun.
 if ((argv.length < 3) || ((argv[2] * testValue) != argv[2])) {
     console.log("Missing number of occurrences");
 } else {
